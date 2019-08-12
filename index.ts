3e94025d2ecd7a2d38e540879b76be9f8537c1e3
Chayim Refael Friedman <friedman@yatmal.co.il>
2019-08-12 19:26:23 +0300

    Create app basic window.

---
+++ index.ts
@@ -0,0 +1,29 @@
+import { BrowserWindow, app } from 'electron';
+
+let wnd: BrowserWindow = null;
+
+function createWindow() {
+  wnd = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  wnd.loadFile('views/index.html');
+  wnd.on('closed', function wndClosed() {
+    wnd = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function allWindowsClosed() {
+  if ('darwin' !== process.platform) {
+    app.quit();
+  }
+});
+
+app.on('activate', function appActivated() {
+  if (null === wnd) {
+    createWindow();
+  }
+});
