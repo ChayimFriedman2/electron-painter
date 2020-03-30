bbed0f6fa4ace4a836927b0be03e1ddb11bb5454
Chayim Refael Friedman <chayimfr@gmail.com>
2020-03-30 21:05:15 +0300

    Add type checking

--- views/index.js
+++ views/index.js
@@ -1,16 +1,9 @@
-(function(window, document) {
-  'use strict';
+const surface = /** @type {HTMLCanvasElement} */ (document.getElementById('surface'));
 
-  const surface = document.getElementById('surface');
+function resizeSurface() {
+  surface.width = window.innerWidth;
+  surface.height = window.innerHeight;
+}
 
-  function resizeSurface() {
-    surface.width = window.innerWidth;
-    surface.height = window.innerHeight;
-  }
-
-  resizeSurface();
-  window.addEventListener('resize', resizeSurface);
-
-  const Painter = require('./scripts/painter');
-  new Painter(surface);
-})(window, document);
+resizeSurface();
+window.addEventListener('resize', resizeSurface);
